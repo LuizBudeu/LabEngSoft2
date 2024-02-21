11d810349ad668e7ef7ac9876d06bfb3eef57e2c
lab <lab@labsoft.local>
2024-02-21 14:12:57 -0300

    removing commented code

--- front/src/HelloWorld.js
+++ front/src/HelloWorld.js
@@ -4,16 +4,6 @@ import axios from 'axios';
 function HelloWorld() {
   const [message, setMessage] = useState('');
 
-  // useEffect(() => {
-  //   axios.get('http://localhost:8000/api/hello-world/')
-  //     .then(response => {
-  //       setMessage(response.data.message);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }, []);
-
   useEffect(() => {
     axios.post('http://localhost:8000/api/token/', {
       username: 'admin',
