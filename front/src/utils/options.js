929060c467c33ed3ba3686a0436b0cbd6975b689
DamaralHenrique <rique01101999@gmail.com>
2024-03-13 14:08:54 -0300

    ✨ Adiciona esquema de escolhas e cores

---
+++ front/src/utils/options.js
@@ -0,0 +1,13 @@
+export const GenderOptions = {
+    0: "Feminino",
+    1: "Masculino",
+    2: "Não binário",
+    3: "Prefiro não informar",
+    4: "Outro"
+};
+
+export const DiabetesOptions = {
+    0: "Não possui",
+    1: "Tipo 1",
+    2: "Tipo 2"
+};
