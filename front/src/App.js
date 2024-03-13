68e5fa18bae77ccdd56b7440d6640ce49ceb48f8
Luiz Budeu <lugui@usp.br>
2024-03-13 15:34:06 -0300

    tabs basicas do medico

--- front/src/App.js
+++ front/src/App.js
@@ -5,23 +5,25 @@ import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
 import { Login } from "./interface/login";
 import { Home } from "./interface/home";
 import { Perfil } from "./interface/paciente/perfil";
-import { NavBar } from "./components/navbar"
+import { NavBar } from "./components/navbar";
 import { SignIn } from "./components/signin";
 import { EditPerfil } from "./interface/paciente/perfil_edit";
+import { MedicoHome } from "./interface/medico/home";
 
 function App() {
-  return (
-    <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signin" element={<SignIn/>}/>
-      <Route path="/paciente/perfil" element={<Perfil/>}/>
-      <Route path="/paciente/perfil/edit" element={<EditPerfil/>}/>
-    </Routes>
-  </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/paciente/perfil" element={<Perfil />} />
+                <Route path="/paciente/perfil/edit" element={<EditPerfil />} />
+                <Route path="/medico" element={<MedicoHome />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
