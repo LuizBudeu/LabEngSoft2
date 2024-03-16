11bb6714cad82cd9101fa47c907bdc7eb8310d56
vinicius-CL <vinicius_lopes15@usp.br>
2024-03-16 02:00:38 -0300

    component da lista para agenda

--- front/src/interface/preparador/agendaTab.js
+++ front/src/interface/preparador/agendaTab.js
@@ -1,26 +1,25 @@
 import React from "react";
 import "./styles/Tabs.css";
-import { GetAgenda } from "../../contoller/preparador/AgendaController";
 import { groupByDate } from "../../utils/group";
+import { AgendaList } from "../../components/agendaList";
 
-const appointments = [
-    {name: 'V1', horario: '2024-03-20'},
-    {name: 'V2', horario: '2024-03-20'},
-    {name: 'V3', horario: '2024-03-21'},
-    {name: 'V4', horario: '2024-03-21'},
-    {name: 'V5', horario: '2024-03-22'},
-]
+const mockedAgenda = [
+    {name: "Vinicius", horario: "2024-03-21 15:00:00"},
+    {name: "Henrique", horario: "2024-03-21 16:00:00"},
+    {name: "Luis", horario: "2024-03-22 13:00:00"},
+    {name: "Felipe", horario: "2024-03-23 10:00:00"},
+    {name: "Rafael", horario: "2024-03-23 11:00:00"},
+];
 
 export const AgendaTab = () => {
     // const [agenda] = GetAgenda("1", "2024-03-20", "2024-03-26");
-    const appts = groupByDate(appointments);
-    console.log(appts);
+    const appts = groupByDate(mockedAgenda);
 
     return(
         <div className="main-container">
             <div className="vertical-box">
-                <span>Horário agendado - Fulano</span>
-                <span>Horário agendado - Fulano</span>
+                <h2>Suas consultas</h2>
+                <AgendaList appointments={appts}/>
             </div>
             <div className="vertical-box">
                 <span>Informações do fulano</span>
--- front/src/utils/group.js
+++ front/src/utils/group.js
@@ -1,11 +1,16 @@
+import { getBaseDate } from "./date";
+
 export const groupByDate = (appointments) => {
     const grouped = {};
+
     appointments.forEach(appt => {
-        const horario = appt.horario;
+        const horario = getBaseDate(appt.horario);
+        console.log(horario)
         if(!grouped[horario]) {
             grouped[horario] = [];
         }
         grouped[horario].push(appt);
     });
+
     return grouped;
-}
+};
