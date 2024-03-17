caf910153ef932ac274272c9424125fbfa550a74
DamaralHenrique <rique01101999@gmail.com>
2024-03-17 16:48:25 -0300

    Adiciona containers customizados

---
+++ front/src/components/backgroundContainer.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { Colors } from "../utils/colors";
+
+export const BackgroundContainer = styled.div`
+    background-color: ${Colors.BackgroundGray};
+    min-height: 100vh;
+`
---
+++ front/src/components/mainContainer.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Colors } from "../utils/colors";
+
+export const MainContainer = styled.div`
+    margin: 32px;
+    padding: 16px;
+    border-radius: 10px;
+    background-color: ${Colors.White};
+`
