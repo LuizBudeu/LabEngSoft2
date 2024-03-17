49c88d5231d05cd972d1089e15974cccac431cb1
DamaralHenrique <rique01101999@gmail.com>
2024-03-17 16:52:19 -0300

    Atualiza utils

--- front/src/utils/date.js
+++ front/src/utils/date.js
@@ -1,6 +1,53 @@
 
+const Meses = {
+    1: "Janeiro",
+    2: "Fevereiro",
+    3: "Março",
+    4: "Abril",
+    5: "Maio",
+    6: "Junho",
+    7: "Julho",
+    8: "Agosto",
+    9: "Setembro",
+    10: "Outubro",
+    11: "Novembro",
+    12: "Dezembro"
+};
+
+const DiaDaSemana = {
+    0: "Domingo",
+    1: "Segunda-feira",
+    2: "Terça-feira",
+    3: "Quarta-feira",
+    4: "Quinta-feira",
+    5: "Sexta-feira",
+    6: "Sábado"
+};
+
 export const getBaseDate = (dateString) => dateString.split(' ')[0];
 
 export const getHourFromDate = (dateString) => dateString.split(' ')[1];
 
 export const getLabelDay = (dateString) => (new Date(dateString)).toDateString();
+
+export const GetHourMinute = (strDateTime, minDuration=60) => {
+    const dateInit = new Date(strDateTime);
+    const dateEnd = new Date(dateInit.getTime() + minDuration*60000);
+    let minutesInit = dateInit.getMinutes();
+    if(minutesInit < 10){
+        minutesInit = "0" + minutesInit;
+    }
+    let minutesEnd = dateEnd.getMinutes();
+    if(minutesEnd < 10){
+        minutesEnd = "0" + minutesEnd;
+    }
+    return(dateInit.getHours() + "h" + minutesInit + " - " + dateEnd.getHours() + "h" + minutesEnd)
+}
+
+export const FormatDate = (strDate) => {
+    const date = new Date(strDate)
+    console.log(strDate);
+    console.log(date);
+    console.log("--------------");
+    return(DiaDaSemana[date.getUTCDay()] + ", " + date.getUTCDate() + " de " + Meses[date.getUTCMonth()+1] + " de " + date.getUTCFullYear())
+}
--- front/src/utils/utils.js
+++ front/src/utils/utils.js
@@ -1,41 +1,7 @@
-const Meses = {
-    1: "Janeiro",
-    2: "Fevereiro",
-    3: "Março",
-    4: "Abril",
-    5: "Maio",
-    6: "Junho",
-    7: "Julho",
-    8: "Agosto",
-    9: "Setembro",
-    10: "Outubro",
-    11: "Novembro",
-    12: "Dezembro"
+export const AppointmentStatus = {
+    agendada: 0,
+    cancelada: 1,
+    realizada: 2,
+    vencida: 3,
+    pendente: 4
 };
-
-const DiaDaSemana = {
-    0: "Domingo",
-    1: "Segunda-feira",
-    2: "Terça-feira",
-    3: "Quarta-feira",
-    4: "Quinta-feira",
-    5: "Sexta-feira",
-    6: "Sábado"
-};
-
-export const GetHourMinute = (strDateTime) => {
-    const date = new Date(strDateTime)
-    let minutes = date.getMinutes();
-    if(minutes < 10){
-        minutes = "0" + minutes
-    }
-    return(date.getHours() + "h" + minutes)
-}
-
-export const FormatDate = (strDate) => {
-    const date = new Date(strDate)
-    console.log(strDate);
-    console.log(date);
-    console.log("--------------");
-    return(DiaDaSemana[date.getUTCDay()] + ", " + date.getUTCDate() + " de " + Meses[date.getUTCMonth()+1] + " de " + date.getUTCFullYear())
-}
