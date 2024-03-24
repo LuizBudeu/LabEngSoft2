115ac8df3ca92a3c108a3d56ae82dedaa5dc1520
vinicius-CL <vinicius_lopes15@usp.br>
2024-03-24 19:20:58 -0300

    api dos treinos

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,8 @@ from .views.paciente.agenda import agenda as paciente_agenda
 from .views.paciente.perfil import perfil as paciente_perfil
 from .views.paciente.perfil import update_perfil as paciente_update_perfil
 from .views.preparador.agenda import agenda as preparador_agenda
+from .views.preparador.workout import workouts
+from .views.preparador.workout import create as workout_create
 
 urlpatterns = [
     path('hello-world/', hello_world, name='hello_world'),
@@ -20,6 +22,8 @@ urlpatterns = [
     path('paciente/perfil', paciente_perfil, name='paciente_perfil'),
     path('paciente/update_perfil', paciente_update_perfil, name='paciente_update_perfil'),
     path('preparador/agenda/', preparador_agenda, name='preparador_agenda'),
+    path('preparador/create_workout/', workout_create, name='workout_create'),
+    path('preparador/workouts', workouts, name='workouts'),
 
     path('token/',  
         jwt_views.TokenObtainPairView.as_view(), 
