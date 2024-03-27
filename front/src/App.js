4574cf6a7a7987eb76c1b37644d7661614d5f94f
Rafael Nakata <rafaelk.nakata@gmail.com>
2024-03-27 19:33:09 -0300

    Implementa Login e Logoff; falta mandar o access_token nas requests para a API

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
 
 import "./App.css";
-import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
-import { Login } from "./interface/login";
-import { Home } from "./interface/home";
-import { NavBar } from "./components/navbar";
-import { SignIn } from "./components/signin";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PacienteHome } from "./interface/paciente/home";
-import { MedicoHome } from "./interface/medico/home";
 import { PreparadorHome } from "./interface/preparador/preparadorHome";
 import { NutricionistaHome } from "./interface/nutricionista/home";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signin" element={<SignIn/>}/>
         <Route path="/paciente" element={<PacienteHome/>}/>
         <Route path="/preparador" element={<PreparadorHome />} />
         <Route path="/nutricionista" element={<NutricionistaHome />} />
