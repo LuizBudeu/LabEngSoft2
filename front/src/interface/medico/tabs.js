80fc9dea75b2531058458316f5866823e4810343
Luiz Budeu <lugui@usp.br>
2024-03-29 16:07:32 -0300

    retirando tab perfil do medico

--- front/src/interface/medico/tabs.js
+++ front/src/interface/medico/tabs.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import AgendaTab from "./AgendaTab";
-import PerfilTab from "./PerfilTab";
 import ExamesTab from "./ExamesTab";
 import { Colors } from "../../utils/colors";
 import "./styles/Tabs.css";
@@ -8,29 +7,20 @@ import { SecondaryNavBar } from "../../components/secondaryNavBar";
 import { MainContainer } from "../../components/mainContainer";
 import { BackgroundContainer } from "../../components/backgroundContainer";
 
-
 const Tabs = () => {
     const [activeTab, setActiveTab] = useState("tab1");
 
     const tabs = [
-        {id: 'tab1', displayName: 'Agenda'},
-        {id: 'tab2', displayName: 'Exames'},
-        {id: 'tab3', displayName: 'Perfil'},
-
+        { id: "tab1", displayName: "Agenda" },
+        { id: "tab2", displayName: "Exames" },
     ];
 
     return (
         <BackgroundContainer>
-            <SecondaryNavBar
-                tabs={tabs}
-                activeTab={activeTab}
-                setActiveTab={setActiveTab}
-            />
+            <SecondaryNavBar tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
             <MainContainer>
                 {activeTab === "tab1" && <AgendaTab />}
                 {activeTab === "tab2" && <ExamesTab />}
-                {activeTab === "tab3" && <PerfilTab />}
-
             </MainContainer>
         </BackgroundContainer>
     );
