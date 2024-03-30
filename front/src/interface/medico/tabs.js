0ccb5db2da77341670134b7de430ed9a107b94cf
Rafael Nakata <rafaelk.nakata@gmail.com>
2024-03-30 14:30:03 -0300

    implementa login para o médico

--- front/src/interface/medico/tabs.js
+++ front/src/interface/medico/tabs.js
@@ -5,14 +5,30 @@ import "./styles/Tabs.css";
 import { SecondaryNavBar } from "../../components/secondaryNavBar";
 import { MainContainer } from "../../components/mainContainer";
 import { BackgroundContainer } from "../../components/backgroundContainer";
+import { useLogout } from "../../utils/useLogout";
+import { ENVIROMENT } from "../../utils/utils";
+import { useLogin } from "../../utils/useLogin";
+
+const LOGIN_URL = process.env.REACT_APP_MEDICO_LOGIN_URL;
+const AUTH_SECRET = process.env.REACT_APP_MEDICO_AUTH_SECRET;
 
 const Tabs = () => {
     const [activeTab, setActiveTab] = useState("tab1");
+    const loggedIn = useLogin(AUTH_SECRET);
+    const logout = useLogout();
 
     const tabs = [
         { id: "tab1", displayName: "Agenda" },
         { id: "tab2", displayName: "Exames" },
     ];
+
+    if (ENVIROMENT === "prod" && !loggedIn)
+        return (
+            <>
+                <p>Bem-vindo ao portal do médico!</p>
+                <a href={LOGIN_URL}>Registre-se ou faça Login.</a>
+            </>
+        );
 
     return (
         <BackgroundContainer>
@@ -21,6 +37,7 @@ const Tabs = () => {
                 {activeTab === "tab1" && <AgendaTab />}
                 {activeTab === "tab2" && <ExamesTab />}
             </MainContainer>
+            <button onClick={logout}>Logout</button>
         </BackgroundContainer>
     );
 };
