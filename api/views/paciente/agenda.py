bbebe9a22e4882fedd366e3f0a5255fa2538b0a0
DamaralHenrique <rique01101999@gmail.com>
2024-04-02 16:20:57 -0300

    Adiciona ParseError

--- api/views/paciente/agenda.py
+++ api/views/paciente/agenda.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
 
 from api.models import Consulta, Usuario
 from datetime import datetime
