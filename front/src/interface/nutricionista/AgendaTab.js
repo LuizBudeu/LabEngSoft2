f38f707cfee58982f46790de53562b5d69e02c33
FelipeArrais <felipearrais2002@gmail.com>
2024-04-03 11:16:25 -0300

    nutricionista dieta/agenda interfaces (wip)

--- front/src/interface/nutricionista/AgendaTab.js
+++ front/src/interface/nutricionista/AgendaTab.js
@@ -4,20 +4,22 @@ import { AgendaList } from "../../components/agendaList";
 import { Row } from "../../components/row";
 import { RowItem } from "../../components/rowItem";
 import { VerticalLine } from "../../components/verticalLine";
-import { GetConsultas } from "../../contoller/nutricionista/AgendaController";
+import { GetConsultas, GetProfile } from "../../contoller/nutricionista/AgendaController";
 import { GetHourMinute } from "../../utils/date";
 import { CustomButton } from "../../components/customButton";
 import { CenterContent } from "../../components/centerContent";
 import { PopUpContainer } from "../../components/popUpContainer";
+import { MainContainer } from "../../components/mainContainer";
+import { EditPerfil } from "./DietaForm";
 
 export const AgendaTab = () => {
-    const [agenda] = GetConsultas("1" /* A SER PREENCHIDO COM ID DO NUTRICIONISTA APÓS INTEGRAÇÃO */,
-                                  "2024-01-01", "3024-01-01");
+    const test_id = "3" // TODO substituir por user_id do usuário logado na integração
+    const [agenda] = GetConsultas(test_id, "2024-01-01", "3024-01-01");
 
     const [selectedAppointment, setSelectedAppointment] = useState("");
     
     return(
-        <>
+        <div>
             <Row>
                 <RowItem grow noPadding>
                     <div style={{width: "100%"}}>
@@ -45,19 +47,29 @@ export const AgendaTab = () => {
                     }
                 </RowItem>
             </Row>
-        </>
+        </div>
     );
 };
 
 const ConsultaInfo = ({consulta}) => {
+    const test_id = 3 // TODO trocar por id do usuário logado na integração
+    const [userProfile, setUserProfile] = GetProfile(test_id);
+    const [showConsultaPopUp, setShowConsultaPopUp] = useState(false);
+
     return (
         <div>
+            <PopUpContainer showPopUp={showConsultaPopUp} closePopUp={() => setShowConsultaPopUp(false)}>
+                <MainContainer>
+                    <EditPerfil closePopUp={() => setShowConsultaPopUp(false)} mainUserId={test_id} setMainUserProfile={setUserProfile}/>
+                </MainContainer>
+            </PopUpContainer>
+
             <div style={{flexGrow: 1, width: "100%", padding: "16px"}}>
                 <body>Paciente: {consulta.paciente__nome}</body>
                 <body>Horário da consulta: {GetHourMinute(consulta.horario, consulta.duracao)}</body>
             </div>
             <Row>
-                <CustomButton title="     Realizar consulta     " onClick={() => console.log("Realizar consulta")} type="primary" />
+                <CustomButton title="Realizar consulta" onClick={() => setShowConsultaPopUp(true)} type="primary" />
             </Row>
             <br/>
             <Row>
@@ -67,7 +79,7 @@ const ConsultaInfo = ({consulta}) => {
             <br/>
             <Row>
                 <CustomButton title="Solicitar exame" onClick={() => console.log("Pedido de exame")} type="primary" />
-                <CustomButton title="Detalhes do paciente" onClick={() => console.log("Detalhes, detalhes")} type="secondary" />
+                <CustomButton title="Detalhes do paciente" onClick={() => console.log("Detalhes, detalhes")} type="primary" />
             </Row>
         </div>
     );
