2b65812f8f8ee9c6df83ef35d89f2edb2842a97e
Rafael Nakata <rafaelk.nakata@gmail.com>
2024-04-16 18:34:39 -0300

    Remove o client_type e refatora

--- tarifacao/cliente/lib/useAxiosWithToken.js
+++ tarifacao/cliente/lib/useAxiosWithToken.js
@@ -4,20 +4,16 @@ import { useIsLoggedIn } from "./useIsLoggedIn";
 
 const API_PROTOCOL_HOSTNAME_PORT = import.meta.env.VITE_API_PROTOCOL_HOSTNAME_PORT;
 
-export const useAxiosWithToken = (client_type) => {
+export const useAxiosWithToken = () => {
     const [cookies] = useCookies();
-    const loggedIn = useIsLoggedIn(client_type);
+    const loggedIn = useIsLoggedIn();
 
     const axiosWithToken = axios.create();
 
-    axiosWithToken.interceptors.request.use(function (config) {
-        config.baseURL = API_PROTOCOL_HOSTNAME_PORT;
-        return config;
-    });
-
     if (loggedIn) {
-        axiosWithToken.interceptors.request.use(function (config) {
-            config.headers = { ...config.headers, Authorization: 'Bearer ' + cookies["access_token"] }
+        axiosWithToken.interceptors.request.use(config => {
+            config.headers.Authorization = 'Bearer ' + cookies["access_token"]
+            config.baseURL = API_PROTOCOL_HOSTNAME_PORT;
             return config;
         });
     }
