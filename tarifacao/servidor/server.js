784e4d2ccd10038f525d14128853eb4d49397a91
Rafael Nakata <rafaelk.nakata@gmail.com>
2024-04-17 08:08:13 -0300

    only use CORS on development

--- tarifacao/servidor/server.js
+++ tarifacao/servidor/server.js
@@ -4,10 +4,13 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const loadSettings = require('./utils').loadSettings
 const getOid = require('./utils').getOid
+require('dotenv').config()
+
 const app = express()
 const port = 3000
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+if (process.env.MODE === 'DEV')
+    app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 app.use(cookieParser())
 
 const countStart = (new Date()).toLocaleDateString('pt-BR', {
